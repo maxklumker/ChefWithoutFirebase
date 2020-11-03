fa5fa8f405937a0795f1d4c9da19b226a91cd394
Max Klumker <maximilian.klumker@code.berlin>
2020-11-03 12:15:01 +0100

    Initial Commit

---
+++ ChefV3WithoutFirebase/ChefV3WithoutFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefV3WithoutFirebaseApp.swift
+//  ChefV3WithoutFirebase
+//
+//  Created by Maximilian Klumker on 03.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ChefV3WithoutFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
